6ed99ce44499c83dc63222e7fda35574563940c0
Grzegorz Ciołek <contact@ciolek.co.uk>
2024-11-03 20:41:14 +0000

    Moved to express.js

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("*", (req, res) => {
+  let url = (
+    req.url.endsWith("/") ? path.join(req.url, "index") : req.url
+  ).slice(1);
+
+  res.render(url, {});
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
+
+export default (req, res) => {
+  return app(req, res);
+};
