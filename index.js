0a665e03d560fed84e4c41ba26a0fab063146b7f
Grzegorz Ciołek <contact@ciolek.co.uk>
2025-02-05 13:31:19 +0000

    Moved to layout based system

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { render } from "ejs";
 import express from "express";
+import expressEjsLayouts from "express-ejs-layouts";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -11,6 +12,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(expressEjsLayouts);
 
 app.set("view engine", "ejs");
 
