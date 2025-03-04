7d8a2b531689494c6104bd536c05b8de2fe20157
Grzegorz Ciołek <contact@ciolek.co.uk>
2025-03-04 18:58:08 +0000

    fix: header alignment on navless layouts

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,33 +4,34 @@ interface HeaderProps {
 
 function NavButton() {
   return (
-    <div className="navbar-start sm:w-fit ml-2 lg:hidden">
-      <label
-        htmlFor="nav-drawer"
-        className="btn btn-ghost btn-square px-0 drawer-button mr-4"
+    <label
+      htmlFor="nav-drawer"
+      className="btn btn-ghost btn-square px-0 drawer-button mr-4"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="h-7 w-7 stroke-current"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="h-7 w-7 stroke-current"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </label>
-    </div>
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        ></path>
+      </svg>
+    </label>
   );
 }
 
 export default function Header({ hideNav }: HeaderProps) {
   return (
     <header className="z-30 sticky top-0 navbar bg-base-100 shadow-lg lg:px-4">
-      {!hideNav ? <NavButton /> : <></>}
+      <div className="navbar-start sm:w-fit ml-2 lg:hidden">
+        {!hideNav ? <NavButton /> : <></>}
+      </div>
+
       <div className="navbar-center md:navbar-start">
         <span className="text-4xl font-bold align-middle text-left print:pl-0 sm:block hidden">
           Grzegorz Ciołek
